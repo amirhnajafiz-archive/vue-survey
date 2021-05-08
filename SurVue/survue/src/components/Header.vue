ca5b591fe9045a7308d0e5a0f2162e757a1072c0
9831065 <najafizadeh21@gmail.com>
2021-05-08 10:43:18 +0430

    responsive header bug fixed

--- SurVue/survue/src/components/Header.vue
+++ SurVue/survue/src/components/Header.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="websiteheader">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button
+        class="navbar-toggler toggler-btn"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        style="margin:0 auto;"
+        @click="popupshow=!popupshow"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div v-show="popupshow" class="nav-popup"> 
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <div :class="['nav-boxing', { active: currentRouteName == '/' }]">
+              <router-link to="/" custom v-slot="{ navigate }">
+                <a @click="navigate" role="link">Home</a>
+              </router-link>
+            </div>
+          </li>
+          <li class="nav-item">
+            <div
+              :class="['nav-boxing', { active: currentRouteName == '/survey' }]"
+            >
+              <router-link to="/survey" custom v-slot="{ navigate }">
+                <a @click="navigate" role="link">Survey</a>
+              </router-link>
+            </div>
+          </li>
+        </ul>
+      </div>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item">
@@ -62,6 +95,7 @@ export default {
     return {
       userURL: "",
       show: false,
+      popupshow: false,
     };
   },
   computed: {
@@ -126,5 +160,28 @@ export default {
 .URL-span:hover {
   background-color: #40c98d;
   color: #ffffff;
+}
+
+.toggler-btn {
+  background-color: #40c98d;
+  color: #ffffff;
+}
+
+.toggler-btn:hover {
+  background-color: #2f9166;
+}
+
+.nav-popup {
+  display: flex;
+  clear: both;
+  width: 100%;
+  justify-content: center;
+  margin: 20px 0;
+}
+
+@media screen and (min-width: 1000px) {
+  .nav-popup {
+    display: none;
+  }
 }
 </style>
